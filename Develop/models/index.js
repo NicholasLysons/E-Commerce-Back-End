286c70dabf4c4116e85b6f84f78aef715e7c32d1
Nicholas Davis Lysons <stmpkjunkie@Nicholass-MacBook-Air.local>
2023-05-23 14:50:45 -0700

    adjusted product.js and file structure

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,16 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToMany(Category, {})
 // Categories have many Products
-
+Category.haveToMany(Product, {})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag)
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(ProductTag)
 module.exports = {
   Product,
   Category,
